4e320e2d3851f03102276527bd827edbdcf66500
Jeff Walker <Jeff@WalkerCodeRanger.com>
2019-09-15 04:15:13 -0700

    add Shape example

---
+++ ExhaustiveMatching.Examples/Shape.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using ExhaustiveMatching;
+using TestNamespace;
+
+class ShapeExample
+{
+    void Example(Shape shape)
+    {
+        switch (shape)
+        {
+            case Square square:
+                Console.WriteLine("Square: " + square);
+                break;
+            case Circle circle:
+                Console.WriteLine("Circle: " + circle);
+                break;
+            case EquilateralTriangle equilateralTriangle:
+                Console.WriteLine("EquilateralTriangle: " + equilateralTriangle);
+                break;
+            case Triangle triangle:
+                Console.WriteLine("Triangle: " + triangle);
+                break;
+            case string s:
+                Console.WriteLine("string: " + s);
+                break;
+            default:
+                throw ExhaustiveMatch.Failed(shape);
+        }
+    }
+}
+
+namespace TestNamespace
+{
+    [Closed(typeof(Square), typeof(Circle), typeof(Triangle))]
+    public abstract class Shape { }
+
+    public class Square : Shape { }
+
+    public class Circle : Shape { }
+
+    // abstract to show abstract leaf types are checked
+    public abstract class Triangle : Shape { }
+
+    public class EquilateralTriangle : Triangle { }
+
+    public class IsoscelesTriangle : Triangle { }
+}
