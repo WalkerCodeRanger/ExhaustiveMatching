8191f5c9bd001f024c8fdf1cddf44f81df066906
Jeff Walker <Jeff@WalkerCodeRanger.com>
2019-09-29 16:26:36 -0700

    upgrade package for examples and add example of mirror hierarchy

---
+++ ExhaustiveMatching.Examples/MirrorHierarchy.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using ExhaustiveMatching;
+using TestNamespace;
+
+class MirrorExample
+{
+    void Example(IAnimal animal)
+    {
+        switch (animal)
+        {
+            case ICat cat:
+                Console.WriteLine("Cat: " + cat);
+                break;
+            case IDog dog:
+                Console.WriteLine("Dog: " + dog);
+                break;
+            default:
+                throw ExhaustiveMatch.Failed(animal);
+        }
+    }
+}
+
+namespace TestNamespace
+{
+    [Closed(typeof(ICat), typeof(IDog))]
+    public interface IAnimal { }
+    public interface ICat : IAnimal { }
+    public interface IDog : IAnimal { }
+
+    public abstract class Animal : IAnimal { }
+    public class Cat : Animal, ICat { }
+    public class Dog : Animal, IDog { }
+}
