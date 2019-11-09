b9060dfde2e36efd22566cc742c8526533f9737a
Jeff Walker <Jeff@WalkerCodeRanger.com>
2019-11-09 09:30:18 -0800

    Add place example

---
+++ ExhaustiveMatching.Examples/Place.cs
@@ -0,0 +1,26 @@
+﻿using ExhaustiveMatching;
+
+namespace TestPlaceNamespace
+{
+    [Closed(typeof(ValuePlace), typeof(DiscardPlace))]
+    public abstract class Place
+    {
+    }
+
+    public class DiscardPlace : Place
+    {
+    }
+
+    [Closed(typeof(VariablePlace), typeof(FieldPlace))]
+    public abstract class ValuePlace : Place
+    {
+    }
+
+    public class VariablePlace : ValuePlace
+    {
+    }
+
+    public class FieldPlace : ValuePlace
+    {
+    }
+}
